0bb4de006fcbd8b1642582607e4623d086d4b19f
sakko <saklism@gmail.com>
2016-01-23 13:11:51 +0700

    added dynamic title

---
+++ README.md
@@ -0,0 +1,10 @@
+# Covering
+
+- StaticPages
+- Dynamic Title
+- Path and Linkings
+- Using Helpers
+- Rspec Testing
+    - controller
+    - helpers
+    - views
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def about
+    @title = "About"
   end
 
   def contact
+    @title = "Contact"
   end
 end
